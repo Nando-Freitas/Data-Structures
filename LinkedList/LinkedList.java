9785ddb186ded06677d70f51d46a214fcb7d3ac6
Nando-Freitas <nando.freitas01@gmail.com>
2022-06-01 23:41:08 -0300

    LinkedList Node

---
+++ LinkedList/LinkedList.java
@@ -0,0 +1,19 @@
+
+class LinkedList {
+
+    Node head;
+
+    class Node {
+        String name;
+        int age;
+        String telephone;
+        Node next;
+
+        Node(String name, int age, String telephone){
+            name = name;
+            age = age;
+            telephone = telephone;
+        }
+    }
+
+}
