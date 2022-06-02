37ce4f7b20bede7079c5c619c75994fe1cfc9d2e
Nando-Freitas <nando.freitas01@gmail.com>
2022-06-02 00:22:35 -0300

    Insertion

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -16,4 +16,25 @@ class LinkedList {
         }
     }
 
+    public static void LinkedList insert(LinkedList list, String name, int age, String telephone){
+
+        //Cria um novo nó
+        Node newNode = new Node(name, age, telephone);
+        newNode.next = null;
+
+        //Se a lista ligada é vazia então torne este nó como cabeça
+        if(list.head == null){
+            list.head = newNode;
+        }else{
+            //Senão, percorra a lista até a último nó e insira o novo nó nela
+            Node last = list.head;
+            while(last.next != null){
+                last = last.next;
+            }
+            //Insere um novo nó no último nó
+            last.next = newNode;
+        }
+        //Devolva a lista
+        return list;
+    }
 }
